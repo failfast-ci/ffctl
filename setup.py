a5ab4ee48995b380af8e41960a6188e4b34d429b
Antoine Legrand <2t.antoine@gmail.com>
2017-06-14 13:50:56 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='ffctl',
-    version='0.1.0',
+    version='0.1.1',
     description="ffctl cli",
     long_description="ffctl py-cli",
     author="Antoine Legrand",

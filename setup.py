5538f0ce6c75bb2992759ea22687a2e93992431d
Antoine Legrand <2t.antoine@gmail.com>
2017-10-19 20:07:03 +0200

    Add py3 compat

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = ["flake8", "pytest", "pytest-cov", "yapf"]
 
 setup(
     name='ffctl',
-    version='0.2.0',
+    version='0.2.1',
     description="ffctl cli",
     long_description="ffctl py-cli",
     author="Antoine Legrand",
